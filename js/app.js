8908ffa5e4e532bcdf9f27e7012bb45b593e98c0
simon01092024 <simonkudus510@gmail.com>
2024-01-29 05:25:13 -0800

    DOM MenuLab (Deliverable) Part 1

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+// Menu data structure (see Task 3.0)
+const menuLinks = [
+    { text: 'about', href: '/about' },
+    { text: 'catalog', href: '/catalog' },
+    { text: 'orders', href: '/orders' },
+    { text: 'account', href: '/account' },
+];
+
 // Task 1.0
 
 // Select and cache the <main> element in a variable named mainEl.
@@ -32,17 +40,25 @@ mainEl.classList.add('flex-ctr')
 
 // Select and cache the <nav id="top-menu"> element in a variable named topMenuEl.
 
+const topMenuEl = document.querySelector('#top-menu');
+
 // Task 2.1
 
 // Set the height topMenuEl element to be 100%.
+
+topMenuEl.style.height = '100%';
 
 // Task 2.2
 
 // Set the background color of topMenuEl using the --top-menu-bg CSS custom property.
 
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+
 // Task 2.3
 
 // Add a class of flex-around to topMenuEl.
+
+topMenuEl.classList.add('flex-around');
 
 // Do Progress Check! 
 
@@ -59,20 +75,20 @@ mainEl.classList.add('flex-ctr')
 // ];
 // Task 3.1
 
-// Iterate over the entire menuLinks array and for each "link" object:
+menuLinks.forEach(function(link) {
+    // Iterate over the entire menuLinks array and for each "link" object:
+    const a = document.createElement('a');
+    // Create an <a> element.
+    a.href = link.href;
+    // On the new element, add an href attribute with its value set to the href property of the "link" object.
+    a.textContent = link.text;
+    // Set the new element's content to the value of the text property of the "link" object. 
+    topMenuEl.appendChild(a);
+    // Append the new element to the topMenuEl element.
+});
 
-// Create an <a> element.
 
-// On the new element, add an href attribute with its value set to the href property of the "link" object.
-
-// Set the new element's content to the value of the text property of the "link" object.
-
-// Append the new element to the topMenuEl element.
-
-
-
-
-
+makeMenu();
 
 
 // Final Progess Check Congrats!
